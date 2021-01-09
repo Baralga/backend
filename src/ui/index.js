828bd3608da05c269a93fa6f22b91bde4a276696
Jan Stamer <jan.stamer@freenet.de>
2021-01-09 23:42:44 +0100

    Set url from filter using stimulus controller.

--- src/ui/index.js
+++ src/ui/index.js
@@ -2,6 +2,7 @@ import { Turbo } from "@hotwired/turbo"
 import { Application } from "stimulus"
 import TrackerController from "./controllers/tracker_controller"
 import ActivityNewController from "./controllers/activity-new_controller"
+import ActivityFilterUrlController from "./controllers/activity-filter-url_controller"
 import NavbarController from "./controllers/navbar_controller"
 
 import baralga_48px from './baralga_48px.png';
@@ -11,4 +12,5 @@ const application = Application.start();
 // register controllers
 application.register("tracker", TrackerController);
 application.register("activity-new", ActivityNewController);
+application.register("activity-filter-url", ActivityFilterUrlController);
 application.register("navbar", NavbarController);
