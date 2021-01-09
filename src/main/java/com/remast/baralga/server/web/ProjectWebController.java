b7f9e556adac24321ecd093be8eea4c0912d3d5c
Jan Stamer <jan.stamer@freenet.de>
2021-01-09 22:59:05 +0100

    Handle activity forms using turbo streams.

--- src/main/java/com/remast/baralga/server/web/ProjectWebController.java
+++ src/main/java/com/remast/baralga/server/web/ProjectWebController.java
@@ -11,24 +11,15 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.time.Duration;
-import java.util.stream.Collectors;
 
 @Transactional
 @Controller
@@ -98,9 +89,8 @@ public class ProjectWebController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping(value = "/projects", produces = "text/html; turbo-stream=*")
-    public String createProject(@Valid ProjectModel projectModel, Model model, BindingResult bindingResult, HttpServletRequest request, HttpServletResponse response) {
-        var project = projectService.create(projectModel.map());
-        model.addAttribute("project", project);
+    public String createProject(@Valid @ModelAttribute("project") ProjectModel projectModel) {
+        projectService.create(projectModel.map());
         return "redirect:/projects"; // NOSONAR
     }
 
