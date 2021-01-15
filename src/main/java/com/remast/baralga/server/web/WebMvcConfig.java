fafae38876877924e9ef5ee014f7ca916a601a1f
Jan Stamer <jan.stamer@freenet.de>
2021-01-15 18:16:05 +0100

    Fixed turbo update.

--- src/main/java/com/remast/baralga/server/web/WebMvcConfig.java
+++ src/main/java/com/remast/baralga/server/web/WebMvcConfig.java
@@ -1,11 +1,15 @@
 package com.remast.baralga.server.web;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.servlet.Filter;
 import java.util.concurrent.TimeUnit;
@@ -26,5 +30,22 @@ public class WebMvcConfig implements WebMvcConfigurer {
         var shallowEtagHeaderFilter = new ShallowEtagHeaderFilter();
         shallowEtagHeaderFilter.setWriteWeakETag(true);
         return shallowEtagHeaderFilter;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(
+            name = {"thymeleafTurboViewResolver"}
+    )
+    ThymeleafViewResolver thymeleafTurboViewResolver(ThymeleafProperties properties, SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine);
+        resolver.setCharacterEncoding(properties.getEncoding().name());
+        resolver.setContentType("text/vnd.turbo-stream.html");
+        resolver.setProducePartialOutputWhileProcessing(properties.getServlet().isProducePartialOutputWhileProcessing());
+        resolver.setExcludedViewNames(properties.getExcludedViewNames());
+        resolver.setViewNames(properties.getViewNames());
+        resolver.setOrder(2147483644);
+        resolver.setCache(properties.isCache());
+        return resolver;
     }
 }
